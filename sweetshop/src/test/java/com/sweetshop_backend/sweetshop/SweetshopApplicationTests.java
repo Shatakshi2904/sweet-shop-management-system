58e32001524cd9a5a766e3f2f70405537fb74195
Shatakshi Sharma <sharma2904shatakshi@gmail.com>
2025-12-14 11:41:27 +0530

    initial commit

---
+++ sweetshop/src/test/java/com/sweetshop_backend/sweetshop/SweetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetshop_backend.sweetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sweetshop/src/test/java/com/sweetshop_backend/sweetshop/auth/controller/AuthControllerTest.java
@@ -0,0 +1,23 @@
+package com.sweetshop_backend.sweetshop.auth.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldRegisterUserSuccessfully() throws Exception {
+        mockMvc.perform(
+                post("/api/auth/register")
+        ).andExpect(status().isCreated());
+    }
+}
